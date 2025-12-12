f1b89a6ad44026b770e0203a419ec184e1f812f2
totallybananas7 <m.github@thejunipers.me.uk>
2025-12-12 18:05:09 +0000

    Commit 5: Started player movement stuff

--- FishingGame MAIN.py
+++ FishingGame MAIN.py
@@ -54,7 +54,7 @@ def main_menu_loop():
                     Main_menu_running = False #stops main menu
                     main_menu_background_music.fadeout(6500) #fades music out over 6.5 seconds
                     fadeout() #goes to the fadeout subroutine for a smooth transition
-                    load_game_background_loop() #loads game background
+                    main_game() #loads game background
 
             if 530<=x<=750 and 493<=y<=541:
                 main_menu_font_colourS = "Green"
@@ -88,31 +88,40 @@ def fadeout(fadespeed=1): #defines fadeout function to have a smooth transition 
         pygame.display.update() #updates the game
         pygame.time.delay(20) #waits 20ms before continuing to the next loop
 
-def load_game_background_loop():
-    game_background_running = True
-    Sky = pygame.image.load("Assets/Background stuff/background_day_sunny.png").convert_alpha() #loads bg
-    Foreground = pygame.image.load("Assets/Background stuff/foreground.png").convert_alpha() #loads fg
+def main_game(): #all game stuff goes in here
+    game_running = True #sets main game loop
 
-    cloud1 = pygame.image.load("Assets/Background stuff/cloud_sunny_1.png").convert_alpha() #loads cloud
-    cloud_1_x = 0 #sets cloud start x pos
-    cloud_1_y = random.randint(1,25) #sets cloud start y pos
+#-----------SPRITE LOADING------------
+    Sky = pygame.image.load("Assets/Background stuff/background_day_sunny.png").convert_alpha()  # loads bg
+    Foreground = pygame.image.load("Assets/Background stuff/foreground.png").convert_alpha()  # loads fg
 
+# ---------- loading cloud sprites -----------
+    cloud1 = pygame.image.load("Assets/Background stuff/cloud_sunny_1.png").convert_alpha()  # loads cloud
+    cloud_1_x = 0  # sets cloud start x pos
+    cloud_1_y = random.randint(1, 25)  # sets cloud start y pos
     cloud2 = pygame.image.load("Assets/Background stuff/cloud_sunny_2.png").convert_alpha()
     cloud_2_x = 500
-    cloud_2_y = random.randint(35,65)
-
+    cloud_2_y = random.randint(35, 65)
     cloud3 = pygame.image.load("Assets/Background stuff/cloud_sunny_3.png").convert_alpha()
     cloud_3_x = 1000
-    cloud_3_y = random.randint(75,95)
+    cloud_3_y = random.randint(75, 95)
+# ---------- loading cloud sprites end -----------
 
-    while game_background_running:
+# ---------- loading player sprites --------------
+
+
+# -----------SPRITE LOADING END-----------
+
+    #------------ MAIN GAME LOOP ---------------
+    while game_running:
         screen.blit(Sky, (0, 0))
         screen.blit(Foreground, (0, 0))
 
-        screen.blit(cloud1,(cloud_1_x,cloud_1_y)) #displays cloud
-        cloud_1_x+=1 #moves cloud 1 pixel left
-        if cloud_1_x>=1280: #checks to see if cloud to far right off screen
-            cloud_1_x = 0 #moves cloud back to the start
+# ---------- loading cloud sprites -----------
+        screen.blit(cloud1, (cloud_1_x, cloud_1_y))  # displays cloud
+        cloud_1_x += 1  # moves cloud 1 pixel left
+        if cloud_1_x >= 1280:  # checks to see if cloud to far right off screen
+            cloud_1_x = 0  # moves cloud back to the start
 
         screen.blit(cloud2, (cloud_2_x, cloud_2_y))
         cloud_2_x += 0.4
@@ -123,8 +132,9 @@ def load_game_background_loop():
         cloud_3_x += 0.8
         if cloud_3_x >= 1280:
             cloud_3_x = 0
+# ------------ loading cloud sprites -----------
 
-        for event in pygame.event.get(): #tells me where mouse is clicked, allows X to quit game without error
+        for event in pygame.event.get():  # tells me where mouse is clicked, allows X to quit game without error
             if event.type == pygame.MOUSEBUTTONDOWN:
                 menu_mouse_pos_test = pygame.mouse.get_pos()
                 print("Mouse clicked at", menu_mouse_pos_test)
@@ -132,17 +142,62 @@ def load_game_background_loop():
                 pygame.quit()
                 exit()
 
-        pygame.display.update()
-        Clock.tick(FPS)
+
 
 def player_movement():
-    player_movement_left = [pygame.image.load("Assets/Character sprite sheet/male_left_stand.png")
-                            pygame.image.load("Assets/Character sprite sheet/male_1_left_walk.png")
-                            pygame.image.load("Assets/Character sprite sheet/male_2_left_walk.png")
-                            pygame.image.load("Assets/Character sprite sheet/male_3_left_walk.png")
-                            pygame.image.load("Assets/Character sprite sheet/male_4_left_walk.png")
 
-                            ]
+    player_sprites= [pygame.image.load("Assets/Character sprite sheet/male_left_stand.png").convert_alpha(), #creates list of sprites
+                    pygame.image.load("Assets/Character sprite sheet/male_1_left_walk.png").convert_alpha(), #1
+                    pygame.image.load("Assets/Character sprite sheet/male_2_left_walk.png").convert_alpha(), #2
+                    pygame.image.load("Assets/Character sprite sheet/male_3_left_walk.png").convert_alpha(), #3
+                    pygame.image.load("Assets/Character sprite sheet/male_4_left_walk.png").convert_alpha(), #4
+                    pygame.image.load("Assets/Character sprite sheet/male_right_stand.png").convert_alpha(), #5
+                    pygame.image.load("Assets/Character sprite sheet/male_1_right_walk.png").convert_alpha(), #6
+                    pygame.image.load("Assets/Character sprite sheet/male_2_right_walk.png").convert_alpha(), #7
+                    pygame.image.load("Assets/Character sprite sheet/male_3_right_walk.png").convert_alpha(), #8
+                    pygame.image.load("Assets/Character sprite sheet/male_4_right_walk.png").convert_alpha()] #9
+
+    p_sprite_count = 5 #creates value to iterate through the animation list above, with the player facing right once spawned
+    player_speed = 30
+    player_x_coordinate = 100
+
+    run = True
+    while run:
+        for event in pygame.event.get(): #handles quitting
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+
+            key = pygame.key.get_pressed() #records the keyboard/mouse for any inputs
+
+            if key[pygame.K.d]: #if D is pressed...
+                player_sprite_count += 1 #update sprite by one
+                if player_sprite_count > 9: #if sprite has done a full walk cycle...
+                    player_sprite_count = 6 #set back to first step of cycle
+
+            elif key[pygame.K.a]:
+                player_sprite_count += 1
+                if player_sprite_count > 4:
+                    player_sprite_count = 1
+
+            else: #if the player is not moving...
+                if player_sprite_count >= 1:
+                    if player_sprite_count <=4:
+                        player_sprite_count = 0
+                elif player_sprite_count >=6:
+                    if player_sprite_count <=9:
+                        player_sprite_count = 5
+
+            screen.blit(player_sprites[p_sprite_count], (player_x_coordinate, 300))
+
+    pygame.display.update()
+    Clock.tick(FPS)
+
+
+
+
+
+
 
 # run main menu
 main_menu_loop()
