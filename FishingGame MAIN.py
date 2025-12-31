b211d1c8e2f8626a55925e8c77e2462303d9b919
totallybananas7 <m.github@thejunipers.me.uk>
2025-12-31 11:58:33 +0000

    Commit 9 - Milestone 5 completion

--- FishingGame MAIN.py
+++ FishingGame MAIN.py
@@ -16,11 +16,13 @@ pygame.display.set_caption("Fishing Game") #sets the title for the game in the w
 FPS = 60 #sets frames per second
 Clock = pygame.time.Clock()
 
-# ------------------------------------------------------------------------------------------------------------------
 #TIPS:
 #USE FORWARD SLASHES FOR FILE PATHS
 #REMEMBER FOR LOCATIONS IT GOES DEST:(X,Y)
 #WHEN LOADING GAME SPRITES, USE THE .convert_alpha() AT THE END TO CONVERT TO A FILE TYPE PYGAME LIKES MORE
+#spritename = pygame.transform.scale(spritename, (x,y)) will scale the sprite size
+
+# Main menu and fadeout
 
 def main_menu_loop():
     Main_menu_running = True
@@ -80,8 +82,6 @@ def main_menu_loop():
         pygame.display.update()
         Clock.tick(FPS)
 
-# ------------------------------------------------------------------------------------------------------------------
-
 def fadeout(fadespeed=1): #defines fadeout function to have a smooth transition and sets the speed of it to 1
     fade = pygame.Surface((1280, 720)) #creates blank surface the size of the screen
     fade.fill((0, 0, 0)) #fills entire surface to black
@@ -91,23 +91,22 @@ def fadeout(fadespeed=1): #defines fadeout function to have a smooth transition 
         pygame.display.update() #updates the game
         pygame.time.delay(20) #waits 20ms before continuing to the next loop
 
-#--------------------------------------------------------------------------------------------------------------------
-
+# Main game
 def main_game(): #all game stuff goes in here
     game_running = True #sets main game loop
 
-#-----------GENERAL SPRITE LOADING------------
+# Main game loading - sprites and variable set up
 
     Foreground = pygame.image.load("Assets/Background stuff/foreground.png").convert_alpha()  # loads fg
 
-    weather_list = ["clear","raining","snowing","fog"] #sets the weathers
+    weather_list = ["Clear","Raining","Snowing","Fog"] #sets the weathers
     current_weather = weather_list[0] #sets default weather to clear
-    weather_duration = random.randint(180000,420000) #creates the weather duration variable used in the while loop between 3 and 7 minutes
+    weather_duration = random.randint(8000,8001) #creates the weather duration variable used in the while loop between 3 and 7 minutes
     last_weather_tick = pygame.time.get_ticks()
 
     day = True #sets time to day
-    daynight_duration = 600000 #sets how long a day and a night is (600000 is 10 minutes)
-    last_daynight_tick = pygame.time.get_ticks()
+    daynight_duration = 16000 #sets how long a day and a night is (600000 is 10 minutes)
+    last_clock_tick = pygame.time.get_ticks()
 
     Sunny_Sky_Day = pygame.image.load("Assets/Background stuff/background_day_sunny.png").convert_alpha()
     Rainy_Sky_Day = pygame.image.load("Assets/Background stuff/background_day_rain.png").convert_alpha()
@@ -211,21 +210,53 @@ def main_game(): #all game stuff goes in here
     current_background_music_index = -1 #creates variable to store current music playing's position for the bg music's index
     new_background_music_index = 0 #creates variable to store the next music's index in the bg music list
 
+    #creating fonts for the inv and loading images
+    board = pygame.image.load("Assets/Menus/board.png").convert_alpha()
+    hotbar = pygame.image.load("Assets/Menus/hotbar.png").convert_alpha()
+    Weight_font = pygame.font.Font("PressStart2P-Regular.ttf", 10)
+    Money_font = pygame.font.Font("PressStart2P-Regular.ttf", 10)
+    Clock_font = pygame.font.Font("PressStart2P-Regular.ttf", 30)
+    Weather_font = pygame.font.Font("PressStart2P-Regular.ttf", 20)
+    Weather_font_colour = "aqua"
+    Fishdex_font = pygame.font.Font("PressStart2P-Regular.ttf", 9)
 
-# -----------GENERAL SPRITE LOADING END-----------
+    minutes = [00, 10, 20, 30, 40, 50] #list of possible minutes the clock can display
+    minute_hand = 00  # creates value for minute list above, starting at 00
+    hour_hand = 6  # sets hour to 6AM
+    clock_tick_length = 8333  # 8.33s per ingame 10 minutes, means each full day is 20m long (8333mm is 8.33s)
+    minute_display = "00" #sets default minute display to 00
+    hour_display = "6" #sets default hour display to 6AM
 
-# ------------ MAIN GAME LOOP ---------------
+    inventory_open = False #creates variable used later for checking if the inv is open
+    inventory = pygame.image.load("Assets/Menus/inventory.png").convert_alpha() #loads inventory bg
+    inventory_font = pygame.font.Font("PressStart2P-Regular.ttf", 20) #loads font for txt in inv
+
+# MAIN GAME LOOP
     while game_running:
 
-#----------------- WEATHER START -------------
+# Weather and time
         current_time = pygame.time.get_ticks() #gets current game tick
 
-        if current_time - last_daynight_tick >= daynight_duration: #if time passed is equal to daynight length
-            last_daynight_tick = current_time
-            if day == True:
-                day = False #turn to night
+        if current_time - last_clock_tick >= clock_tick_length: #if enough time has passed
+            last_clock_tick = current_time #resets timer
+
+            minute_hand +=1 #update minute hand every 8.33s
+
+            if minute_hand > 5: #if an ingame hour has passed..
+                minute_hand = 00
+                minute_display = str(minutes[minute_hand]) #displays clocks minutes as the location of the item in the minutes list based off the minute_hand variable
+                hour_hand +=1 #updates hour
+
+                if hour_hand > 23: #if midnight
+                    hour_hand = 00 #resets hour count
+
+            minute_display = str(minutes[minute_hand]) #displays clock minutes
+            hour_display = str(hour_hand) #displays clock hour
+
+            if 6<= hour_hand < 18: #if between 6am and 6pm, day is true
+                day = True
             else:
-                day = True #turn to day
+                day = False
 
         if current_time - last_weather_tick >= weather_duration: #if the time passed is equal to the random weather duration
             last_weather_tick = current_time #resets timer
@@ -238,21 +269,23 @@ def main_game(): #all game stuff goes in here
                 new_weather = random.choice(weather_list) #if it hasn't, it reselects again
             current_weather = new_weather #updates weather
 
-            if current_weather == "clear": #updating background
-                if day == True:
+            if current_weather == "Clear": #updating background
+                if day == True: #if its day, set sprites to daytime variation
                     weather_bg = Sunny_Sky_Day
                     cloud1 = cloud_sunny_1
                     cloud2 = cloud_sunny_2
                     cloud3 = cloud_sunny_3
                     new_background_music_index = 0 #sets new bg music
+                    Weather_font_colour = "aqua" #sets weather display GUI font colour to aqua
                 else:
-                    weather_bg = Clear_Sky_Night
+                    weather_bg = Clear_Sky_Night #else its night, updates sprites to nighttime variation
                     cloud1 = cloud_sunny_1
                     cloud2 = cloud_sunny_2
                     cloud3 = cloud_sunny_3
                     new_background_music_index = 4
+                    Weather_font_colour = "aqua"
 
-            elif current_weather == "raining":
+            elif current_weather == "Raining":
                 if day == True:
                     weather_bg = Rainy_Sky_Day
                     cloud1 = cloud_rainy_1
@@ -260,6 +293,7 @@ def main_game(): #all game stuff goes in here
                     cloud3 = cloud_rainy_3
                     rain = True
                     new_background_music_index = 1
+                    Weather_font_colour = "dodgerblue"
                 else:
                     weather_bg = Rainy_Sky_Night
                     cloud1 = cloud_rainy_1
@@ -267,42 +301,47 @@ def main_game(): #all game stuff goes in here
                     cloud3 = cloud_rainy_3
                     rain = True
                     new_background_music_index = 5
+                    Weather_font_colour = "dodgerblue"
 
-            elif current_weather == "fog":
+            elif current_weather == "Fog":
                 if day == True:
                     weather_bg = FogSnow_Sky_Day
                     fog = True
                     new_background_music_index = 2
+                    Weather_font_colour = "azure3"
                 else:
                     weather_bg = FogSnow_Sky_Night
                     fog = True
                     new_background_music_index = 6
+                    Weather_font_colour = "azure3"
 
-            elif current_weather == "snowing":
+            elif current_weather == "Snowing":
                 if day == True:
                     weather_bg = FogSnow_Sky_Day
                     snow = True
                     new_background_music_index = 3
+                    Weather_font_colour = "azure"
                 else:
                     weather_bg = FogSnow_Sky_Night
                     snow = True
                     new_background_music_index = 7
+                    Weather_font_colour = "azure"
 
 
         screen.blit(weather_bg,(0,0))
         screen.blit(Foreground,(0,0))
 
-        if current_weather == "clear" or current_weather == "raining":
+        if current_weather == "Clear" or current_weather == "Raining":
             screen.blit(cloud1,(cloud_1_x,cloud_1_y))  # displays cloud
             cloud_1_x += 1  # moves cloud 1 pixel left
             if cloud_1_x >= 1280:  # checks to see if cloud to far right off-screen
                 cloud_1_x = 0  # moves cloud back to the start
-        if current_weather == "clear" or current_weather == "raining":
+        if current_weather == "Clear" or current_weather == "Raining":
             screen.blit(cloud2,(cloud_2_x,cloud_2_y))
             cloud_2_x += 0.4
             if cloud_2_x >= 1280:
                 cloud_2_x = 0
-        if current_weather == "clear" or current_weather == "raining":
+        if current_weather == "Clear" or current_weather == "Raining":
             screen.blit(cloud3,(cloud_3_x,cloud_3_y))
             cloud_3_x += 0.8
             if cloud_3_x >= 1280:
@@ -326,10 +365,7 @@ def main_game(): #all game stuff goes in here
             current_background_music.play(-1) #play new music
             current_background_music_index = new_background_music_index #updates tracker
 
-
-#------------------ WEATHER END ---------------------
-
-#------------------ player movement -----------------
+# Player movement and input
 
         for event in pygame.event.get(): #handles quitting and debug code
             if event.type == pygame.MOUSEBUTTONDOWN:
@@ -380,7 +416,53 @@ def main_game(): #all game stuff goes in here
                 player_sprite_count = 5
 
         screen.blit(player_sprites[player_sprite_count], (player_x_coordinate, 265)) #put the player on screen and update the sprite
-#-------- player movement end -------------
+
+
+# Inventory/GUI
+
+        if key[pygame.K_e]: #if the player opens the inventory
+            inventory_open = True
+
+        if inventory_open == True:
+            dim_overlay = pygame.Surface((1280, 720)) #creates a new screen the size of the window
+            dim_overlay.set_alpha(140) #sets transparency of the new screen
+            dim_overlay.fill((0, 0, 0)) #sets the new screen to black
+            screen.blit(dim_overlay, (0, 0)) #puts the faded new screen to make the background darker to bring more contrast to inv
+
+            #displays inv and text
+            screen.blit(inventory,(160,90))
+            screen.blit(inventory_font.render("Money:", False, "yellow"), (175,107))
+            screen.blit(inventory_font.render("Weight:", False, "yellow"), (480,107))
+            screen.blit(inventory_font.render("Fishdex:", False, "yellow"), (780,107))
+            screen.blit(inventory_font.render("Fish:", False, "yellow"), (175,145))
+            screen.blit(inventory_font.render("Quantity:", False, "yellow"), (480,145))
+            screen.blit(inventory_font.render("Sell Price:", False, "yellow"), (780, 145))
+
+            inv_mouse_pos = pygame.mouse.get_pos() #gets mouse pos
+            x, y = inv_mouse_pos #gets x and y variables of mouse pos
+            if 1075<=x<=1110 and 100<=y<=130: #if near the X button
+                if event.type == pygame.MOUSEBUTTONDOWN: #if mouse clicked
+                    inventory_open = False #inv is closed
+
+        else:
+            ingame_clock = hour_display+":"+minute_display
+
+            screen.blit(board,(1080,650)) #bottom right (weight)
+            screen.blit(Weight_font.render("Weight placeholder", False, "yellow"), (1087, 677))
+
+            screen.blit(board,(1080,5)) #top right (time)
+            screen.blit(Clock_font.render(ingame_clock, False, "yellow"),(1100,25))
+
+            screen.blit(board,(1080,75)) #second top right (weather)
+            screen.blit(Weather_font.render(current_weather, False, Weather_font_colour), (1105, 97))
+
+            screen.blit(board,(1080,145)) #third top right (fishdex)
+            screen.blit(Fishdex_font.render("Fishdex placeholder", False, "yellow"),(1092,170))
+
+            screen.blit(board,(10,5)) #top left (money)
+            screen.blit(Money_font.render("Money placeholder", False, "yellow"), (20, 32))
+
+        screen.blit(hotbar,(576,650)) #bottom middle (hotbar)
 
         pygame.display.update()
         Clock.tick(FPS)
